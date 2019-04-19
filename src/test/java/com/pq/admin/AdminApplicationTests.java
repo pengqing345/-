ccdb2fa951ac860abc5d1618154f38dc9a3d0c5b
pq <1044047070@qq.com>
2019-04-19 16:25:03 +0800

    update

--- src/test/java/com/pq/admin/AdminApplicationTests.java
+++ src/test/java/com/pq/admin/AdminApplicationTests.java
@@ -1,10 +1,7 @@
 package com.pq.admin;
 
-import com.pq.pojo.User;
-import com.pq.service.AdminService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,15 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class AdminApplicationTests {
 
-    @Autowired
-    AdminService adminService;
 
     @Test
     public void contextLoads() {
-        User u = new User();
-        u.setUserName("admin");
-        u.setPassword("0192023a7bbd73250516f069df18b500");
-        System.out.println(adminService.admin(u));
+        activitiUtils.creatTable();
     }
 
 }
